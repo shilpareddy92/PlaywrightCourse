352cfe3b01b38b10d220309da530daa5bdca22ce
Shilpa vallapureddy <shilpavallapureddy@Shilpas-MacBook-Air.local>
2026-06-09 13:01:44 +0530

    any data type

---
+++ TSPractice/any.js
@@ -0,0 +1,17 @@
+"use strict";
+let num = 12.33;
+let something;
+something = "shilpa";
+something = true;
+console.log(num);
+console.log(typeof something); //boolean the öast value
+let array = ["naveen", 100, true];
+array.push("automation");
+array.push(10.3);
+console.log(array);
+function add(a, b) {
+    return a + b;
+}
+console.log(add("shilpa", "reddy"));
+console.log(add("shilpa", 10));
+console.log(add(10, 10));
---
+++ TSPractice/any.ts
@@ -0,0 +1,20 @@
+let num:any=12.33;
+let something :any;
+something ="shilpa";
+something=true;
+
+
+console.log(num);
+console.log(typeof something);//boolean the öast value
+let array:any=["naveen", 100, true];
+array.push("automation");
+array.push(10.3);
+console.log(array);
+
+function add(a:any, b:any):any{
+    return a+b;
+}
+console.log(add("shilpa", "reddy"));//shilpareddy
+console.log(add("shilpa", 10));//shilpa10
+console.log(add(10, 10));//20
+
